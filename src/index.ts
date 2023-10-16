8871644122f09e70b2e5c9d6c26252754c812331
Carlos Pallarés <cpallarest@gmail.com>
2023-10-16 12:58:29 +0200

    Hello World working

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+const PORT: number = 3000;
+
+app.get("/", (req, res)=>{
+    res.send("Hola Mundo")
+})
+
+app.get("/bba", (req,res)=>{
+    res.send("Perico palotes");
+})
+
+app.listen(PORT, ()=>{
+    return console.log(`App is listening at por: ${PORT}`)
+})
+
